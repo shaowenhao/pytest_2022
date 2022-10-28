b9cea1be4e444d19e58dbb1128be33450182b526
Wenhao Shao Z00498TA <wenhao.shao@siemens.com>
2022-10-28 10:31:59 +0800

    yield生成器 返回用例原始数据

--- tool/read_file.py
+++ tool/read_file.py
@@ -20,9 +20,32 @@ class ReadFile:
             content = yaml.load(doc, Loader=yaml.Loader)
             return content
 
+    @classmethod
+    #根据case的定义 把case名加到内容部分的字典内容里
+    def read_case(cls, path):
+        case_data = cls.read_yaml(path)
+        for k,v in case_data.items():
+            case_name = k
+            if v['is_run']:
+                # 字典里多了一个K,V 'case_name': '获取运单号'
+                v['case_name'] = case_name
+                # 不要一次性返回所有的值 不用return 用yield
+                #print(v)
+                yield v
+
 if __name__ == '__main__':
     # D:\PycharmProjects\pytest_2022\tool/
     # print(ReadFile.project_directory)
     #
     # print(ReadFile.read_yaml('config/environment.yaml'))
-    print(ReadFile.read_yaml('case/test.yaml'))
+    # print(ReadFile.read_yaml('case/test.yaml'))
+
+    # {'path': '/get_waybill_no', 'method': 'get', 'remark': '获取运单号，提取运单号,这个方法提取出运单号', 'is_run': True, 'data': None,
+    #  'extract_key': {'waybill_no': '$.waybill_no'}, 'assert_expression': ['"lj520"=="$.waybill_no"'],
+    #  'case_name': '获取运单号'}
+    # {'path': '/lu_dan', 'method': 'post', 'remark': '录单，使用运单号', 'is_run': True,
+    #  'data': {'waybill_no': '$.waybill_no', 'lu_dan_ren': '小江'}, 'extract_key': None,
+    #  'assert_expression': ['"运单创建成功"=="$.msg"'], 'case_name': '录单'}
+    case_data = ReadFile.read_case('case/test.yaml')
+    for case in case_data:
+        print(case)
